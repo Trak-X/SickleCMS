d9194ab8b7f5f40263eee56b5aa3b6a3f53d91ca
Simon Sickle <simonsimons34@gmail.com>
2013-10-19 23:36:01 -0400

    add array of allowed extensions

--- resources/config.php
+++ resources/config.php
@@ -7,7 +7,16 @@ return array(
     'list_folders_first' => true,
     'list_sort_order'    => 'natcasesort',
     'theme_name'         => 'bootstrap',
-
+    
+    // Array of allowed extensions
+    'allowed_extensions' => array(
+    	'exe',
+    	'jpg',
+    	'png',
+    	'zip',
+    	'img',
+    ),
+    
     // Hidden files
     'hidden_files' => array(
         '.htaccess',
